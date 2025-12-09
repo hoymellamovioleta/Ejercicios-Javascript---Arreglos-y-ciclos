0fa74af3365807ab7a92d89568d3a8e3a7aa9dd8
Victoria Villagómez <elisse12.mr@gmail.com>
2025-12-09 00:35:02 -0600

    Fix: Se modifica nombre del array

--- Exercise3.js
+++ Exercise3.js
@@ -4,12 +4,14 @@ Write a program to compute the sum and product (multiplication) of an array of
 numbers. Print out the sum and the product. 
 Example: Given an array [1, 2, 3, 4] The sum is 10. The product is 24.
 
-let arr = [1, 2, 3, 4];
+
 */
+let arrTree = [1, 2, 3, 4];
+
 //Sum
-const add = arr.reduce((cont, number) => cont + number, 0);
+const add = arrTree.reduce((cont, number) => cont + number, 0);
 console.log(add);
 
 //Product
-const product = arr.reduce((cont, number) => cont*number,1);
+const product = arrTree.reduce((cont, number) => cont*number,1);
 console.log(product);
